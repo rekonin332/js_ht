3c5292841185fce27112188bd9d50a94d9dbcf38
rekonin332 <38468856@qq.com>
2021-10-15 17:33:53 +0800

    enable tooltip...

--- js/util3.js
+++ js/util3.js
@@ -37,13 +37,13 @@ function Generated_HT_TopoMap(pData) {
     '0020000|士大夫|大分站|拓扑定义-分站|,0040000|kkkk|大分站|拓扑定义-分站|,0070000|443 34其4|KJ306－F(16)H本安型分站|拓扑定义-分站|,0080000|787878787878|大分站|拓扑定义-分站|,0120000|佛挡杀佛东莞市东方|大分站|拓扑定义-分站|,0130000|45545454545|大分站|拓扑定义-分站|,0140000|aabb|大分站|拓扑定义-分站|,0150000|cc|大分站|拓扑定义-分站|,192.168.101.3|7F.09.F6.09.7A.03|网络模块|拓扑定义-网络模块|0020000#0040000#0070000#0080000#0120000#0130000#0140000#0150000,0160000|dddddd|大分站|拓扑定义-分站|,192.168.100.130+|添加分站|分站|拓扑定义-添加分站|,192.168.100.130|7F.DD.11.00.0E.08|网络模块|拓扑定义-网络模块|0160000#192.168.100.130+,0110000|东方大道多多多|大分站|拓扑定义-分站|,192.168.100.203+|添加分站|分站|拓扑定义-添加分站|,192.168.100.203|7F.DD.11.00.0E.07|网络模块|拓扑定义-网络模块|0110000#192.168.100.203+,54-EE-75-C0-DE-DA|未定义安装位置|交换机|拓扑定义-交换机|192.168.101.3#192.168.100.130#192.168.100.203,0010000|安装位置A|大分站|拓扑定义-分站|,0060000|方便的肉体和认同|大分站|拓扑定义-分站|,192.168.101.1+|添加分站|分站|拓扑定义-添加分站|,192.168.101.1|7F.09.F6.09.7A.01|网络模块|拓扑定义-网络模块|0010000#0060000#192.168.101.1+,7f.09.F6.09.7A.O1|101.1|交换机|拓扑定义-交换机|192.168.101.1,0030000|333333|大分站|拓扑定义-分站|,0050000|测试测试测试|大分站|拓扑定义-分站|,192.168.101.4+|添加分站|分站|拓扑定义-添加分站|,192.168.101.4|7F.09.F6.09.7A.04|网络模块|拓扑定义-网络模块|0030000#0050000#192.168.101.4+,7f.09.F6.09.7A.O4|104|交换机|拓扑定义-交换机|192.168.101.4,7f.DD.11.00.0E.06|刘德华|交换机|拓扑定义-交换机|192.168.100.204,0090000|烦烦烦|中分站|拓扑定义-分站|,0100000|糖糖糖|大分站|拓扑定义-分站|,192.168.100.204+|添加分站|分站|拓扑定义-添加分站|,192.168.100.204|7F.DD.11.00.0E.06|网络模块|拓扑定义-网络模块|0090000#0100000#192.168.100.204+,00.00.00.00.00.00|添加交换机|交换机|拓扑定义-添加交换机|';
   if (pData == undefined || pData.trim() == '') {
     pData = _data1;
-    //alert("没有数据进来,用的假数据");
   }
   if (pData == undefined || pData.trim() == '') return;
   leftMk = [];
   rightMk = [];
   dataModel = new ht.DataModel();
   graphView = new ht.graph.GraphView(dataModel);
+  graphView.enableToolTip(); //启用ToolTip
   view = graphView.getView();
   view.className = 'main';
   document.body.appendChild(view);
@@ -62,7 +62,7 @@ function Generated_HT_TopoMap(pData) {
     new ht.List([
       new ht.graph.ScrollBarInteractor(graphView),
       new ht.graph.SelectInteractor(graphView),
-      new ht.graph.MoveInteractor(graphView),
+      // new ht.graph.MoveInteractor(graphView),
       new ht.graph.DefaultInteractor(graphView),
       new ht.graph.TouchInteractor(graphView),
     ])
@@ -96,8 +96,7 @@ function Generated_HT_TopoMap(pData) {
   //分类数据
   for (let index = 0; index < _arr.length; index++) {
     const element = _arr[index];
-    // console.log(element);
-    // document.writeln(element + '<br />');
+
     if (
       element.indexOf('拓扑定义-添加分站') > -1 ||
       element.indexOf('拓扑定义-分站') > -1
@@ -106,8 +105,6 @@ function Generated_HT_TopoMap(pData) {
 
       var _name = element.split('|');
       var indexName = _name[0];
-
-      // console.log('fz', indexName + '  >>>>> ' + element);
     }
     if (
       element.indexOf('拓扑定义-交换机') > -1 ||
@@ -117,7 +114,7 @@ function Generated_HT_TopoMap(pData) {
       var _name = element.split('|');
       var indexName = _name[0];
       var lst = _name[_name.length - 1].split('#');
-      // console.log(indexName + '#' + _name[_name.length - 1] + '#');
+
       jhj_lst.push(indexName + '#' + _name[_name.length - 1] + '#');
     }
     if (element.indexOf('拓扑定义-网络模块') > -1) {
@@ -125,14 +122,14 @@ function Generated_HT_TopoMap(pData) {
       var _name = element.split('|');
       var indexName = _name[0];
       var lst = _name[_name.length - 1].split('#');
-      // console.log(indexName + ',' + lst.join(',') + '#');
+
       wl_lst.push(indexName + ',' + lst.join(',') + '#');
     }
     allLst.push(element.trim());
   }
 
   jhj_str = jhj_lst.join('|');
-  // console.log(jhj_str);
+
   //重新生成交换机-模块-分组列表
   for (let index = 0; index < wl_lst.length; index++) {
     const element = wl_lst[index];
@@ -141,7 +138,6 @@ function Generated_HT_TopoMap(pData) {
   }
   console.log('=======');
   console.log(jhj_str);
-  // GenerateTopoMap(_result);
   console.log('************GenerateTopoNew***********');
   GenerateTopoNew(jhj_str);
   //
@@ -204,7 +200,7 @@ function GenerateSubTopo(tuyuanLst, initialX, initialY, xOffset, yOffset) {
       jhj_add_buttion = '00.00.00.00.00.00';
       continue;
     }
-    // console.log(leftStr);
+
     var j_m_f = subStr.split('#');
     let mk_num = 0;
     //j_m_f[0] 交换机名字
@@ -217,19 +213,17 @@ function GenerateSubTopo(tuyuanLst, initialX, initialY, xOffset, yOffset) {
       let fenzhanNodeLst = [];
       for (let k = 1; k < m_f.length; k++) {
         const fz = m_f[k];
-        //function createNode(x, y, name, toolTip, icon)
-        // console.log(fz);
+
         let _toolTip = getToolTipByName(fz); //TODO:
         //生成分站图元
         fz_tuyuan = createNode(x, y, fz, _toolTip, fz_img);
         fenzhanNodeLst.push(fz_tuyuan);
-        // console.log(x, y);
+
         y_total_mk += y;
 
         y += yOffset;
       }
       //生成模块图元
-      // console.log(x, y_total_mk, m_f.length - 1);
       let y_mk = y_total_mk / (m_f.length - 1);
       y_total_jhj += y_mk;
       mk_num += 1;
@@ -244,9 +238,6 @@ function GenerateSubTopo(tuyuanLst, initialX, initialY, xOffset, yOffset) {
       fenzhanNodeLst = [];
       mokuaiNodeLst.push(mk_tuyuan);
     }
-    // console.log(j_m_f);
-    // console.log('kkkkkkk', y_total_jhj, j_m_f.length - 1, mk_num);
-
     //生成交换机图元
     const jhj_tuyuan = createNode(
       x + xOffset * 2,
